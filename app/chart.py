3392c8888d9df11456980d2d1e9999d1601c23b2
kto6655 <kto6655@stern.nyu.edu>
2024-08-08 23:34:48 -0400

    Simplified chart.py code

--- app/chart.py
+++ app/chart.py
@@ -1,51 +1,51 @@
 # Welcome to the "app/chart.py" file...
 
-# Import the HTML data
+# IMPORT PACKAGES
+
 import requests
-
-tiktok_url = "https://ads.tiktok.com/business/creativecenter/keyword-insights/pc/en"
-
-tiktok_raw_data = requests.get(tiktok_url)
-
-tiktok_raw_data
-
-# Translate the HTML data
 from bs4 import BeautifulSoup
-
-tiktok_soup = BeautifulSoup(tiktok_raw_data.text)
-
-tiktok_soup
-
-# Parse the HTML data
-
-tiktok_parse = tiktok_soup.find("script", id="__NEXT_DATA__") 
-
-# Convert to JSON format
-
 import json
-
-tiktok_json = json.loads(tiktok_parse.string)
-
-# Parse JSON data
-
-tiktok_keywords = tiktok_json["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]["keywordList"]
-
-# Convert JSON data into DataFrame
-
 from pandas import DataFrame
-
-keywords_df = DataFrame(tiktok_keywords)
-
-keyword_summary = keywords_df[["keyword","impression","like","share","comment"]]
-
-# Organize table for chart and email 
-
-email_table = keyword_summary.sort_values(by=['impression'], ascending=[False])
-
-# Create bar chart
-
 from plotly.express import bar
 
-bar(x=email_table["keyword"], y=email_table["impression"], title="Top 20 Keywords Trending on TikTok in the United States (Last 7 Days)",
+# FETCH DATA
+
+def fetch_tiktok_data():
+    
+    # import HTML data
+    tiktok_url = "https://ads.tiktok.com/business/creativecenter/keyword-insights/pc/en"
+    tiktok_raw_data = requests.get(tiktok_url)
+
+    # translate HTML data
+    tiktok_soup = BeautifulSoup(tiktok_raw_data.text)
+
+    # parse the HTML data
+    tiktok_parse = tiktok_soup.find("script", id="__NEXT_DATA__") 
+
+    # convert HTML data to JSON format
+    tiktok_json = json.loads(tiktok_parse.string)
+
+    # parse JSON data
+    tiktok_keywords = tiktok_json["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]["keywordList"]
+
+    # convert JSON data into DataFrame
+    keywords_df = DataFrame(tiktok_keywords)
+    keyword_summary = keywords_df[["keyword","impression","like","share","comment"]]
+
+    # sort table by impressions
+    email_table = keyword_summary.sort_values(by=['impression'], ascending=[False])
+
+fetch_tiktok_data
+
+# CHART DATA
+
+keyword_chart = bar(x=email_table["keyword"], y=email_table["impression"], 
+    title="Top 20 Keywords Trending on TikTok in the United States (Last 7 Days)",
     labels={"x":"Keyword", "y":"# of Impressions"})
+
+keyword_chart
+
+
+
+
 
